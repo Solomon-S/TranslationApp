85a4178a79af712a43756865e075ed9b8985c3f2
angelesmarin <angeles.marinbatana@bsu.edu>
2023-10-17 11:11:23 -0400

    Added classes

---
+++ src/main/java/edu/bsu/cs222/TranslatorApp.java
@@ -0,0 +1,8 @@
+package edu.bsu.cs222;
+
+public class TranslatorApp {
+    public static void main(String[] args) {
+        TranslatorApp translatorApp = new TranslatorApp();
+        translatorApp.();
+    }
+}
