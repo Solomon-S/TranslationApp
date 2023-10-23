b4c5b25b358920d446338b66f8e22a9fada5a404
angelesmarin <angeles.marinbatana@bsu.edu>
2023-10-23 01:57:59 -0400

    added test for the TranslatorAPIHandlerTest.java class

--- src/main/java/edu/bsu/cs222/TranslatorApp.java
+++ src/main/java/edu/bsu/cs222/TranslatorApp.java
@@ -6,7 +6,7 @@ public class TranslatorApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Ball State Translation App!");
+        System.out.println("Welcome to the Ball State Translation App");
         System.out.println("Enter word in English to translate to Spanish:");
 
         String input = scanner.nextLine();
