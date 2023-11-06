70daae9038c8f2d8b3de382238663eb3b33f067b
Rees Bomgardner <rees.bomgardner@bsu.edu>
2023-11-06 17:45:54 -0500

    Changed the main method to launch a future instance of the TranslatorAppGui.

--- src/main/java/edu/bsu/cs222/TranslatorApp.java
+++ src/main/java/edu/bsu/cs222/TranslatorApp.java
@@ -1,30 +1,9 @@
 package edu.bsu.cs222;
 
-import java.util.Scanner;
-
-import static edu.bsu.cs222.languageNameToCode.mapLanguageNameToCode;
+import static javafx.application.Application.launch;
 
 public class TranslatorApp {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        try (scanner) {
-            System.out.println("Welcome to the Translation App");
-            System.out.println("Enter a word, phrase, or text in English to translate:");
-            String input = scanner.nextLine();
-            String apiKey = "AIzaSyCefoMSOZ4NATjaLsTutfj_lATTwnURkp0";
-            TranslatorAPIHandler translatorAPIHandler = new TranslatorAPIHandler(apiKey);
-            System.out.println("Enter the target language (e.g., 'Spanish', 'Japanese', 'French'):");
-            String targetLanguageName = scanner.nextLine();
-            String targetLanguage = mapLanguageNameToCode(targetLanguageName);
-            if (targetLanguage == null) {
-                System.err.println("Language not recognized. Exiting.");
-                return;
-            }
-            String translatedText = translatorAPIHandler.translateText(input, "en", targetLanguage);
-            System.out.println("Translation: " + translatedText);
-        } catch (TranslationException e) {
-            System.err.println(e.getMessage());
-        }
+        launch(TranslatorAppGui.class, args);
     }
 }
