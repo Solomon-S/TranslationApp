a52472a437f5f1dc8a79f8031b296072ae33e491
Rees Bomgardner <rees.bomgardner@bsu.edu>
2023-11-06 18:03:25 -0500

    Created the supportedLanguages class. The logic behind the JavaFX interface is now able to show the supported languages.

---
+++ src/main/java/edu/bsu/cs222/supportedLanguages.java
@@ -0,0 +1,12 @@
+package edu.bsu.cs222;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class supportedLanguages {
+    public static final ObservableList<String> supportedLanguages = FXCollections.observableArrayList(
+            "Spanish", "Japanese", "Italian", "Arabic", "Korean", "Catalan", "Simplified Chinese",
+            "Traditional Chinese", "Polish", "Dutch", "Portuguese", "Russian", "French", "Swedish",
+            "German", "Greek", "Telugu", "Gujarati", "Thai", "Hebrew", "Ukrainian", "Hindi", "Urdu", "Vietnamese"
+    );
+}
