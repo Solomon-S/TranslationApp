eb1965c2d496ef47183aabd848abcca44b7afc54
Rees Bomgardner <rees.bomgardner@bsu.edu>
2023-11-06 17:55:26 -0500

    Created the TranslatorAppGui class. The JavaFX interface is created and setup for the creation of the logic of the translation.

---
+++ src/main/java/edu/bsu/cs222/TranslatorAppGui.java
@@ -0,0 +1,24 @@
+package edu.bsu.cs222;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TranslatorAppGui {
+    private Stage primaryStage;
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle("Translation App");
+
+        TranslatorAppLogic translatorApp = new TranslatorAppLogic(this);
+
+        Scene scene = createScene(translatorApp.getRoot());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+    private Scene createScene(Parent root) {
+        return new Scene(root, 1024, 576);
+    }
+}
