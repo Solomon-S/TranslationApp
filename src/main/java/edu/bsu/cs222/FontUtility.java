b7f1034ab445517036ba30216d54e2cc899d3475
Rees Bomgardner <rees.bomgardner@bsu.edu>
2023-11-06 18:17:17 -0500

    Added the FontUtility class. This class will change the fonts on the JavaFX UI to make the application more readable and presentable.

---
+++ src/main/java/edu/bsu/cs222/FontUtility.java
@@ -0,0 +1,23 @@
+package edu.bsu.cs222;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+
+public class FontUtility {
+    public static void setFontSize(Font font, javafx.scene.Node... nodes) {
+        for (javafx.scene.Node node : nodes) {
+            if (node instanceof Labeled) {
+                ((Labeled) node).setFont(font);
+            } else if (node instanceof TextField) {
+                ((TextField) node).setFont(font);
+            } else if (node instanceof ComboBox) {
+                ((ComboBox) node).setStyle("-fx-font-size: " + font.getSize());
+            } else if (node instanceof ListView) {
+                ((ListView) node).setStyle("-fx-font-size: " + font.getSize());
+            }
+        }
+    }
+}
