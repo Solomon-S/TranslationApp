a7beed1c44ad5ab0289e924572c1ca498449b22d
Rees Bomgardner <rees.bomgardner@bsu.edu>
2023-11-13 18:03:11 -0500

    Added language support from Google Translate API for all languages. I also alphabetized the observable list.

--- src/main/java/edu/bsu/cs222/supportedLanguages.java
+++ src/main/java/edu/bsu/cs222/supportedLanguages.java
@@ -5,18 +5,21 @@ import javafx.collections.ObservableList;
 
 public class supportedLanguages {
     public static final ObservableList<String> supportedLanguages = FXCollections.observableArrayList(
-            "Spanish", "Japanese", "Italian", "Arabic", "Korean", "Catalan", "Simplified Chinese",
-            "Traditional Chinese", "Polish", "Dutch", "Portuguese", "Russian", "French", "Swedish",
-            "German", "Greek", "Telugu", "Gujarati", "Thai", "Hebrew", "Ukrainian", "Hindi", "Urdu",
-            "Vietnamese","Burmese", "Afrikaans", "Albanian", "Amharic", "Armenian", "Assamese", "Aymara",
-            "Azerbaijani", "Bambara", "Basque", "Belarusian", "Bengali", "Bhojpuri", "Bosnian", "Bulgarian",
-            "Cebuano", "Corsican", "Croatian", "Czech", "Danish", "Dhivehi", "Dogri", "Esperanto",
-            "Estonian", "Ewe", "Filipino (Tagalog)", "Finnish", "Frisian", "Galician", "Georgian",
-            "Guarani", "Haitian Creole", "Hausa", "Hawaiian", "Hmong", "Hungarian", "Icelandic", "Igbo",
-            "Ilocano", "Indonesian", "Irish", "Javanese", "Kannada", "Kazakh", "Khmer", "Kinyarwanda",
-            "Konkani", "Krio", "Kurdish", "Kurdish (Sorani)", "Kyrgyz", "Lao", "Latin", "Latvian",
-            "Lingala", "Lithuanian", "Luxembourgish", "Macedonian", "Maithili", "Malagasy", "Malay",
-            "Malayalam", "Maltese", "Maori", "Marathi", "Meiteilon (Manipuri)", "Mizo", "Mongolian",
-            "Myanmar (Burmese)"
+            "Afrikaans", "Albanian", "Amharic", "Arabic", "Armenian", "Assamese", "Aymara","Azerbaijani",
+            "Bambara", "Basque", "Belarusian", "Bengali", "Bhojpuri", "Bosnian", "Bulgarian",  "Catalan",
+            "Chinese (Simplified)", "Chinese (Traditional)","Cebuano", "Corsican", "Croatian", "Czech",
+            "Danish", "Dhivehi", "Dogri", "Dutch", "English", "Esperanto", "Estonian", "Ewe", "Filipino (Tagalog)",
+            "Finnish", "French", "Frisian", "Galician", "Georgian", "German","Greek", "Guarani", "Gujarati",
+            "Haitian Creole", "Hausa", "Hawaiian", "Hebrew", "Hindi", "Hmong", "Hungarian", "Icelandic", "Igbo",
+            "Ilocano", "Indonesian", "Irish", "Italian", "Japanese", "Javanese", "Kannada", "Kazakh", "Khmer",
+            "Kinyarwanda", "Konkani", "Korean", "Krio", "Kurdish", "Kurdish (Sorani)", "Kyrgyz", "Lao", "Latin",
+            "Latvian", "Lingala", "Lithuanian", "Luxembourgish", "Macedonian", "Maithili", "Malagasy", "Malay",
+            "Malayalam", "Maltese", "Maori", "Marathi", "Meiteilon (Manipuri)", "Mizo", "Mongolian", "Myanmar (Burmese)",
+            "Nepali", "Norwegian", "Nyanja (Chichewa)", "Odia (Oriya)", "Oromo", "Pashto", "Persian", "Polish",
+            "Portuguese (Portugal, Brazil)", "Punjabi", "Quechua", "Romanian", "Russian", "Samoan", "Sanskrit",
+            "Scots Gaelic", "Sepedi", "Serbian", "Sesotho", "Shona", "Sindhi", "Sinhala (Sinhalese)", "Slovak",
+            "Slovenian", "Somali", "Spanish", "Sundanese", "Swahili", "Swedish", "Tagalog (Filipino)", "Tajik",
+            "Tamil", "Tatar", "Telugu", "Thai", "Tigrinya", "Tsonga", "Turkmen", "Twi (Akan)", "Ukrainian", "Urdu",
+            "Uyghur", "Uzbek", "Vietnamese", "Welsh", "Xhosa", "Yiddish", "Yoruba", "Zulu"
     );
 }
